386fdb84086791ce2998b33d990a643b039fb107
hussam-hussam <67809881+hussam-hussam@users.noreply.github.com>
2022-06-29 14:25:31 +0300

    Update InlineSourceMapFiles2.java

--- src/main/java/burp/core/scanners/InlineSourceMapFiles2.java
+++ src/main/java/burp/core/scanners/InlineSourceMapFiles2.java
@@ -11,14 +11,14 @@ import com.google.re2j.Matcher;
 
 import static burp.utils.Constants.b64SourceMapRegex;
 
-public class InlineSourceMapFiles implements Runnable{
+public class InlineSourceMapFiles2 implements Runnable{
     private static final IBurpExtenderCallbacks callbacks = BurpExtender.getCallbacks();
     private static final IExtensionHelpers helpers = callbacks.getHelpers();
     private final IHttpRequestResponse baseRequestResponse;
     private final Path outputDirectory;
     private final UUID taskUUID;
 
-    public InlineSourceMapFiles(IHttpRequestResponse baseRequestResponse, UUID taskUUID, long timeStamp) {
+    public InlineSourceMapFiles2(IHttpRequestResponse baseRequestResponse, UUID taskUUID, long timeStamp) {
         this.baseRequestResponse = baseRequestResponse;
         this.outputDirectory = Paths.get(System.getProperty("user.home"))
                 .resolve(".BurpSuite")
