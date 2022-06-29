cd1ba05181170c7d50b58adeed95a3c9379a6a51
hussam-hussam <67809881+hussam-hussam@users.noreply.github.com>
2022-06-30 00:30:46 +0300

    Update Secrets2.java

--- src/main/java/burp/core/scanners/Secrets2.java
+++ src/main/java/burp/core/scanners/Secrets2.java
@@ -23,6 +23,7 @@ public class Secrets2 implements Runnable {
         this.baseRequestResponse = baseRequestResponse;
         this.url = url;
         this.taskUUID = taskUUID;
+        System.out.println("Secrets2 Builder.......");
     }
 
     @Override
@@ -39,6 +40,7 @@ public class Secrets2 implements Runnable {
         StringBuilder uniqueMatchesSBHigh = new StringBuilder();
         while (matcherSecrets.find() && BurpExtender.isLoaded()) {
             double entropy = Utilities.getShannonEntropy(matcherSecrets.group(20)); // group(2) matches our secret
+            system.out.println("searching for secrets...........");
             if (entropy >= 3.5) {
                 // if high entropy, confidence is "Firm"
                 uniqueMatchesHigh.add(matcherSecrets.group().getBytes(StandardCharsets.UTF_8));
