97eb5c55c57c2e377976ffd0cf98a3b1ff1cccc9
hussam-hussam <67809881+hussam-hussam@users.noreply.github.com>
2022-06-29 14:22:09 +0300

    Update CloudURLs2.java

--- src/main/java/burp/core/scanners/CloudURLs2.java
+++ src/main/java/burp/core/scanners/CloudURLs2.java
@@ -13,13 +13,13 @@ import static burp.utils.Constants.*;
 import static burp.utils.Utilities.appendFoundMatches;
 import static burp.utils.Utilities.sendNewIssue;
 
-public class CloudURLs implements Runnable {
+public class CloudURLs2 implements Runnable {
     private static final IBurpExtenderCallbacks callbacks = BurpExtender.getCallbacks();
     private static final IExtensionHelpers helpers = callbacks.getHelpers();
     private final IHttpRequestResponse baseRequestResponse;
     private final UUID taskUUID;
 
-    public CloudURLs(IHttpRequestResponse baseRequestResponse, UUID taskUUID) {
+    public CloudURLs2(IHttpRequestResponse baseRequestResponse, UUID taskUUID) {
         this.baseRequestResponse = baseRequestResponse;
         this.taskUUID = taskUUID;
     }
